8c68f35e56a67926c203b3be8a48961aab56bc3d
Evan Jelley <jelleyevan@gmail.com>
2024-09-03 16:26:39 -0600

    Work

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,61 @@ import './App.css'
 import axios from 'axios'
 import { OpenAI } from 'openai'
 
+
 function App() {
 
   return (
-      <>
-        <img src={'./src/assets/hiking.webp'} alt="Hiking" className='background-img' />
+    <>
+      <img src={'./src/assets/musicBackground.png'} alt="Hiking" className='background-img' />
+      <Navbar />
+      <div className='row'>
+        <div className='col-12 col-md-6'>
+          <About />
+        </div>
+        <div className='col-12 col-md-6'>
         <Chatbot />
-      </>
+        </div>
+      </div>
+    </>
   )
 }
+
+const About = () => {
+  return (
+    <>
+      <h1>Songwriting AI</h1>
+      <p>Let's write a song together! Type a message in the chat window to get started.</p>
+      <p>Need some inspiration? Click on one of the idea bubbles below to get started.</p>
+      <p>Powered by OpenAI's GPT-4 model.</p>
+      <p>Created by <a href="https://www.linkedin.com/in/evan-jelley" target="_blank">Evan Jelley</a></p>
+      <p>View the source code on <a href="https://github.com/EvanJelley/SongwritingAI" target="_blank">GitHub</a></p>
+    </>
+  );
+};
+
+const Navbar = () => {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+      <div class="container d-flex justify-content-center">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+              </li>
+          </ul>
+      </div>
+  </nav>
+    );
+  };
+
 
 const Chatbot = () => {
   const openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true })
@@ -42,7 +88,7 @@ const Chatbot = () => {
 
       // Extract the chatbot's response
       console.log(response.choices[0].message);
-      const botMessage = response.choices[0].message.content.trim();
+      const botMessage = response.choices[0].message.content;
 
       // Add the bot's response to the chat
       setMessages([...messages, { sender: 'user', text: input }, { sender: 'bot', text: botMessage }]);
@@ -54,12 +100,24 @@ const Chatbot = () => {
     setInput('');
   };
 
+  function cleanMessage(message) {
+    let formattedMessage = message.replace(/\*\*(.*?)\*\*/g, '<i>$1</i>');
+    formattedMessage = formattedMessage.replace(/###(.*?)\n/g, '<b>$1</b>\n</br>');
+    return { __html: formattedMessage };
+  }
+
+  function ideaHandler(idea) {
+    setInput(idea);
+    handleSend();
+  }
+
   return (
     <div className="chatbot">
       <div className="chat-window">
+        {messages.length === 0 && <IdeaBubbleBay ideadHandler={ideaHandler} />}
         {messages.map((msg, index) => (
           <div key={index} className={`message ${msg.sender}`}>
-            {msg.text}
+            <div className='message-content' dangerouslySetInnerHTML={cleanMessage(msg.text)}></div>
           </div>
         ))}
       </div>
@@ -67,12 +125,87 @@ const Chatbot = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        onKeyPress={(e) => e.key === 'Enter' && handleSend()}
-        placeholder="Type your message..."
+        onKeyDown={(e) => e.key === 'Enter' && handleSend()}
+        placeholder="Let's write a song together!"
       />
-      <button onClick={handleSend}>Send</button>
+      <button className="submit-button" onClick={handleSend}>Send</button>
     </div>
   );
 };
+
+const IdeaBubbleBay = ({ ideadHandler }) => {
+  const ideasSpecific = [
+    'What if we wrote a song about a journey to the moon?',
+    'How about a song about a lost love?',
+    'What if we wrote a song about a summer day at the beach?',
+    'How about a song about a road trip across the country?',
+    'What if we wrote a song about a magical forest?',
+    'How about a song about a broken heart?',
+    'What if we wrote a song about a new beginning?',
+    'How about a song about a rainy day?',
+    'What if we wrote a song about a secret garden?',
+    'How about a song about a childhood memory?',
+    'What if we wrote a song about love?',
+    'How about a song about nature?',
+    'What if we wrote a song about dreams?',
+    'How about a song about hope?',
+    'What if we wrote a song about friendship?',
+    'How about a song about the stars?',
+    'What if we wrote a song about the ocean?',
+    'How about a song about the seasons?',
+    'What if we wrote a song about time?',
+    'How about a song about the universe?',
+  ];
+
+  const ideasGeneral = [
+    'I need a chord progression for a song I am writing.',
+    'I need help with writing lyrics for a song.',
+    'I need help with writing a melody for a song.',
+    'I need help with writing a bridge for a song.',
+    'I need help with writing a chorus for a song.',
+    'I need help with writing a verse for a song.',
+    'I need help with writing a hook for a song.',
+    'I need help with writing a pre-chorus for a song.',
+    'I need help with writing a song title.',
+    'I need help with writing a song intro.',
+    'I need help with writing a song outro.',
+    'I need help with writing a song structure.',
+    'I need help with writing a song arrangement.',
+    'I need help with writing a song form.',
+    'I need help with writing a song concept.',
+  ];
+
+  const randomSpecificIdea1 = ideasSpecific[Math.floor(Math.random() * ideasSpecific.length)];
+  const randomSpecificIdea2 = ideasSpecific[Math.floor(Math.random() * ideasSpecific.length)];
+  const randomGeneralIdea1 = ideasGeneral[Math.floor(Math.random() * ideasGeneral.length)];
+  const randomGeneralIdea2 = ideasGeneral[Math.floor(Math.random() * ideasGeneral.length)];
+
+  return (
+    <div className="idea-bubble-bay">
+      <h2 className="idea-bubble-bay-title">Need some inspiration?</h2>
+      <div className='row'>
+        <div className='col-12 col-md-6'>
+          <ChatIdeaBubble idea={randomSpecificIdea1} ideadHandler={ideadHandler} />
+          <ChatIdeaBubble idea={randomSpecificIdea2} ideadHandler={ideadHandler} />
+        </div>
+        <div className='col-12 col-md-6'>
+          <ChatIdeaBubble idea={randomGeneralIdea1} ideadHandler={ideadHandler} />
+          <ChatIdeaBubble idea={randomGeneralIdea2} ideadHandler={ideadHandler} />
+        </div>
+      </div>
+    </div>
+  );
+
+
+}
+
+const ChatIdeaBubble = ({ idea, ideadHandler }) => {
+  return (
+    <div className="chat-idea-bubble">
+      <button className="idea-button" onClick={() => ideadHandler(idea)}>{idea}</button>
+    </div>
+  );
+}
+
 
 export default App
